98e56eb3cbd4fe6a5ff9052b9da1d3538c4202b9
Atulp24 <apk200209@gmail.com>
2024-10-07 15:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const charFreqCount = {};
+	for(let char of str) {
+		if(charFreqCount[char])
+		{
+			charFreqCount[char] += 1;
+		}
+		else
+		{
+			charFreqCount[char] = 1;
+		}
+	}
+	for(let char of str) {
+		if(charFreqCount[char] === 1)
+		{
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
